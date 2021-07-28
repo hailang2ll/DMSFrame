744ba7c9e4aca5abe7e07c632f01068e3f41403d
hailang2ll <79522860@qq.com>
2021-07-28 09:31:57 +0800

    实例测试

--- DMSFrame.API/Controllers/HomeController.cs
+++ DMSFrame.API/Controllers/HomeController.cs
@@ -6,6 +6,23 @@ using System.Web.Mvc;
 
 namespace DMSFrame.API.Controllers
 {
+    /// <summary>  
+    /// 手动设计一个Person类。用于放到List泛型中  
+    /// </summary>  
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; private set; }
+        public string Sex { get; set; }
+
+        public Person(string name, string sex, int age)
+        {
+            Name = name;
+            Age = age;
+            Sex = sex;
+        }
+    }
+
     /// <summary>
     /// 
     /// </summary>
@@ -17,6 +34,15 @@ namespace DMSFrame.API.Controllers
         /// <returns></returns>
         public ActionResult Index()
         {
+            List<Person> list = new List<Person>();
+            list.Add(new Person("张三", "男", 20));
+            list.Add(new Person("王成", "男", 32));
+            list.Add(new Person("李丽", "女", 19));
+            list.Add(new Person("何英", "女", 35));
+            list.Add(new Person("何英", "女", 18));
+
+            var ls = list.GroupBy(a => a.Name).Select(g => (new { name = g.Key, count = g.Count(), ageC = g.Sum(item => item.Age), sexc = g.Select(q => q.Sex).FirstOrDefault() }));
+
             return Content("OK");
         }
     }
